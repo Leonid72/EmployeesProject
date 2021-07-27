565a1c4078bc90c71af869faea77cb58671dffd8
Leonid <leonid72@gmail.com>
2021-07-27 16:46:40 +0300

    27-07

---
+++ EmployeeWebClient/EmployeeWebClient/Controllers/TestController.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeWebClient.Models;
+using EmployeeWebClient.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeWebClient.ModelBinders;
+namespace EmployeeWebClient.Controllers
+{
+    public class TestController : Controller
+    {
+        Uri uriAddress = new Uri("https://localhost:44328/api");
+        HttpClient httpClient;
+        EmployeeVM empVM;
+
+
+        public TestController()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = uriAddress;
+            empVM = new EmployeeVM();
+        }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            List<Employee> employees = new List<Employee>();
+            HttpResponseMessage httpResponse = httpClient.GetAsync(httpClient.BaseAddress + "/Employee").Result;
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var data = httpResponse.Content.ReadAsStringAsync().Result;
+                empVM.employees = JsonConvert.DeserializeObject<List<Employee>>(data);
+            }
+            return View("Test", empVM);
+        }
+
+
+        [HttpPost]
+        public ActionResult AddEmployee([ModelBinder(typeof(ModelBinders.EmployeeBinder))] Employee emp)
+        {
+            return View("Test", empVM);
+        }
+    }
+}
---
+++ EmployeeWebClient/EmployeeWebClient/ModelBinders/EmployeeBinder.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeWebClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeWebClient.ModelBinders
+{
+    public class EmployeeBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            HttpContextBase objContext = controllerContext.HttpContext;
+            string empFirstName = objContext.Request.Form["employee.FirstName"];
+            string empLastName = objContext.Request.Form["employee.LastName"];
+
+            Employee emp = new Employee();
+            emp.FirstName = empFirstName;
+            emp.LastName = empLastName;
+            return emp;
+        }
+    }
+}
